e19637c2e25dcaad81ca9f69294be4fe827431b1
Wesley Jr <wesleyjr2002@gmail.com>
2022-05-12 21:11:06 -0300

    Add `polybar` configuration

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+CONFIG="$XDG_CONFIG_HOME/polybar/config"
+
+# Terminate already running bar instances
+pkill polybar
+
+# Wait until the processes have been shut down
+while pgrep --uid $UID --exact polybar >/dev/null; do sleep 1; done
+
+# Launch the bar
+polybar --quiet main --config="$CONFIG" &
