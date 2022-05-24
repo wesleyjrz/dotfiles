e29c6c70a6d62998d3dfe7ce0e815c222219a3f0
Wesley Jr <wesleyjr2002@gmail.com>
2022-05-24 16:30:32 -0300

    Update `lualine` configuration

--- .config/nvim/lua/config/lualine.lua
+++ .config/nvim/lua/config/lualine.lua
@@ -2,11 +2,11 @@ require("lualine").setup {
 	options = {
 		icons_enabled = true,
 		theme = "nord",
-		component_separators = { left = "|", right = "|" },
+		component_separators = { left = "", right = "" },
 		section_separators = { left = "", right = "" },
 		disabled_filetypes = {},
 		always_divide_middle = true,
-		globalstatus = false
+		globalstatus = true
 	},
 	sections = {
 		lualine_a = { "mode" },
