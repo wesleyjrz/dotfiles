c690f147ab6c9929cf2f236f799a91c0e70827e6
Wesley Jr <wesleyjr2002@gmail.com>
2022-05-24 18:00:36 -0300

    Update `Comment` configuration

--- .config/nvim/lua/config/comment.lua
+++ .config/nvim/lua/config/comment.lua
@@ -1,21 +1,24 @@
 require("Comment").setup {
-	padding = true,     -- add a whitespace b/w the comment and the line
+	padding = true,     -- add a space b/w the comment and the line
 	toggler = {
 		line = "<C-c>",   -- toggle line-comment
-		block = "<C-A-c>" -- toggle block-comment
 	},
 	opleader = {
 		line = "gc",      -- toggle line-comment
 		block = "gb"      -- toggle block-comment
 	},
+	extra = {
+		above = "gcO",    -- add comment on the line above
+		below = "gco",    -- add comment on the line below
+		eol = "gcA",      -- add comment at the end of line
+	},
 	mappings = {
 		-- Operator-pending mapping
 		-- Includes `gcc`, `gbc`, `gc[count]{motion}` and `gb[count]{motion}`
-		-- NOTE: These mappings can be changed individually by `opleader` and `toggler` config
 		basic = true,
 		-- Extra mapping
 		-- Includes `gco`, `gcO`, `gcA`
-		extra = false,
+		extra = true,
 		-- Extended mapping
 		-- Includes `g>`, `g<`, `g>[count]{motion}` and `g<[count]{motion}`
 		extended = false
