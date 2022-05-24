0cf5d044a5f0bfea39e4d3c0d13f5bbd3b34a726
Wesley Jr <wesleyjr2002@gmail.com>
2022-05-24 16:32:36 -0300

    Update `terminal` configuration, now it will be easier to toggle and navigate, also the line numbers and cursor line are disabled in terminal windows

--- .config/nvim/lua/config/terminal.lua
+++ .config/nvim/lua/config/terminal.lua
@@ -1,8 +1,22 @@
 local key = vim.api.nvim_set_keymap
+local autocmd = vim.api.nvim_create_autocmd
 
--- Exit terminal mode (ESC / Ctrl + g)
-key("t", "<Esc>", "<C-\\><C-n>", {})
-key("t", "<C-g>", "<C-\\><C-n>", {})
+-- Toggle terminal even in terminal mode (Ctrl + Alt + t)
+key("t", "<C-A-t>", "<C-\\><C-n><C-A-t>", {})
+
+-- Navigate from the terminal window (Ctrl + h/j/k/l)
+key("t", "<C-h>", "<C-\\><C-n><C-w>h", {})
+key("t", "<C-j>", "<C-\\><C-n><C-w>j", {})
+key("t", "<C-k>", "<C-\\><C-n><C-w>k", {})
+key("t", "<C-l>", "<C-\\><C-n><C-w>l", {})
+
+-- Terminal window options
+autocmd("TermOpen", {
+	command = [[ startinsert! |
+	             setlocal nonumber |
+	             setlocal norelativenumber |
+	             setlocal nocursorline ]]
+})
 
 -- The following option will hide the buffer when it is closed instead of
 -- deleting it
