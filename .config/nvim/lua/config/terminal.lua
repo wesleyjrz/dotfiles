f536364b1eb79907a1bbc6f3eac44aaf54bca1e8
Wesley Jr <wesleyjr2002@gmail.com>
2022-05-24 15:01:14 -0300

    Update `nvim-terminal` options

--- .config/nvim/lua/config/terminal.lua
+++ .config/nvim/lua/config/terminal.lua
@@ -4,18 +4,16 @@ local key = vim.api.nvim_set_keymap
 key("t", "<Esc>", "<C-\\><C-n>", {})
 key("t", "<C-g>", "<C-\\><C-n>", {})
 
--- following option will hide the buffer when it is closed instead of deleting
--- the buffer. This is important to reuse the last terminal buffer
--- IF the option is not set, plugin will open a new terminal every single time
+-- The following option will hide the buffer when it is closed instead of
+-- deleting it
 vim.o.hidden = true
 
 require("nvim-terminal").setup {
+	toggle_keymap = "<C-A-t>",
 	window = {
 		position = "botright",
 		split = "sp",
 		width = 50,
-		height = 15,
-	},
-	disable_default_keymaps = false,
-	toggle_keymap = "<C-A-t>"
+		height = 10
+	}
 }
