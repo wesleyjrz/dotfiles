6a64a5f059f884b2027477947a5dcb7dd8006211
Wesley Jr <wesleyjr2002@gmail.com>
2022-05-24 21:06:02 -0300

    Add `lualine-lsp-progress`

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -97,7 +97,10 @@ return require("packer").startup(function(use)
 	-- Statusline
 	use {
 		"nvim-lualine/lualine.nvim",
-		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+		requires = {
+			{ "kyazdani42/nvim-web-devicons", opt = true },
+			{ "arkav/lualine-lsp-progress", opt = true }
+		},
 		config = get_config("lualine")
 	}
 
