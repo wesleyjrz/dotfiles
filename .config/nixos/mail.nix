7abbb29eca0caa0370c15b27af9fa2827a7f4787
Wesley Jr <wesleyjr2002@gmail.com>
2022-05-27 17:58:19 -0300

    First `neomutt` configuration

--- .config/nixos/mail.nix
+++ .config/nixos/mail.nix
@@ -11,10 +11,27 @@
 				preNew = "mbsync --all";
 			};
 		};
+		neomutt = {
+			enable = true;
+			vimKeys = true;
+			extraConfig = ''
+			# Main options
+			set quit  # don't confirm when quitting
+			set charset = "utf-8"
+			set tmpdir = "~/.local/cache/mutt"
+			set header_cache = "~/.local/cache/mutt"
+			set message_cache = "~/.local/cache/mutt"
+
+			# Style
+			unset sig_dashes  # no dashes before signature
+			set date_format = "%Y-%m-%d %H:%M"
+			'';
+		};
 	};
 
 	accounts.email = {
 		accounts.gmail = {
+			flavor = "gmail.com"; # set IMAP and SMTP automatically (make sure) IMAP is enabled on Gmail settings
 			address = "wesleyjr2002@gmail.com";
 			maildir = { path = "gmail"; };
 			folders = {
@@ -23,26 +40,22 @@
 				sent   = "sent";
 				trash  = "trash";
 			};
-			# Reserved for gpg configuration
-			imap.host = "gmail.com";
 			mbsync = {
 				enable = true;
 				create = "maildir";
 			};
 			msmtp.enable = true;
 			notmuch.enable = true;
+			neomutt.enable = true;
 			primary = true;
 			realName = "Wesley V. Santos Jr.";
 			signature = {
 				text = ''
-					Wesley V. Santos Jr.
+				••• Wesley V. Santos Jr.
 				'';
 				showSignature = "append";
 			};
-			passwordCommand = "mail-password";
-			smtp = {
-				host = "gmail.com";
-			};
+			passwordCommand = "cat ~/Documents/general/mail-password.txt";
 			userName = "wesleyjr2002@gmail.com";
 		};
 	};
