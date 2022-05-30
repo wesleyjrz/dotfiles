6fd00dd98d80a6c4c074076f2fa75ac60b76d5cd
Wesley Jr <wesleyjr2002@gmail.com>
2022-05-30 18:39:38 -0300

    Refactor `commands.lua`: organise autocommands into augroups

--- .config/nvim/lua/commands.lua
+++ .config/nvim/lua/commands.lua
@@ -13,95 +13,121 @@ local usercmd = vim.api.nvim_create_user_command
 -- Map code runner (Ctrl + Alt + r)
 key("n", "<C-A-r>", "<cmd>doautocmd User<CR>", { silent = true })
 
+local code_runner = augroup("CodeRunner", { clear = false })
+local clean_remaining = augroup("CleanRemaining", { clear = false })
+
 -- C/C++
 autocmd("User Run", {
 	pattern = { "*.c" },
-	command = [[ !gcc -ansi -pedantic -Wall -Wextra % ; ./a.out ]]
+	command = [[ !gcc -ansi -pedantic -Wall -Wextra % ; ./a.out ]],
+	group = code_runner
 })
 
 autocmd("User Run", {
 	pattern = { "*.C", "*.cc", "*.cp", "*.cpp", "*.cxx", "*.c++" },
-	command = [[ !g++ % ; ./a.out ]]
+	command = [[ !g++ % ; ./a.out ]],
+	group = code_runner
 })
 
+-- Clean remaining executables (C and C++ binaries)
 autocmd("BufUnload", {
-	-- Clean remanescent executables
 	pattern = { "*.c", "*.C", "*.cc", "*.cp", "*.cpp", "*.cxx", "*.c++" },
-	command = [[ silent !rm --force a.out ]]
+	command = [[ silent !rm --force a.out ]],
+	group = clean_remaining
 })
 
 -- Shell Script
 autocmd("User Run", {
 	pattern = "*.sh",
-	command = [[ !/bin/sh  % ]]
+	command = [[ !/bin/sh  % ]],
+	group = code_runner
 })
 
 -- Python
 autocmd("User Run", {
 	pattern = "*.py",
-	command = [[ !python3 % ]]
+	command = [[ !python3 % ]],
+	group = code_runner
 })
 
 -- Lua
 autocmd("User Run", {
 	pattern = "*.lua",
-	command = [[ !lua % ]]
+	command = [[ !lua % ]],
+	group = code_runner
 })
+
+-- Run LOVE projects
+usercmd("RunLove", "silent !love %:p:h", {})
 
 -- HTML
 autocmd("User Run", {
 	pattern = "*.html",
-	command = [[ silent !live-server %:p:h & ]]
+	command = [[ silent !live-server %:p:h & ]],
+	group = code_runner
 })
 
 -- CSS (Joke)
 autocmd("User Run", {
 	pattern = { "*.css", "*.scss", "*.sass" },
-	callback = function() print("lol, you can't run a stylesheet") end
+	callback = function() print("lol, you can't run a stylesheet") end,
+	group = code_runner
 })
 
 -- Markdown
 autocmd("User Run", {
 	pattern = "*.md",
-	command = [[ MarkdownPreview ]]
+	command = [[ MarkdownPreview ]],
+	group = code_runner
 })
 
 -- LaTeX
 autocmd("User Run", {
 	pattern = "*.tex",
 	-- Convert tex to pdf and send a SIGHUP signal to mupdf (to update changes)
-	command = [[ silent !pdflatex % -o %.pdf && pkill --signal SIGHUP mupdf ]]
+	command = [[ silent !pdflatex % -o %.pdf && pkill --signal SIGHUP mupdf ]],
+	group = code_runner
 })
 
+-- Clean log and aux files (LaTeX remaining files)
 autocmd("BufUnload", {
 	pattern = "*.tex",
-	-- Clean log and aux files
-	command = [[ silent !rm --force %:p:h/*.log %:p:h/*.aux ]]
+	command = [[ silent !rm --force %:p:h/*.log %:p:h/*.aux ]],
+	group = clean_remaining
 })
 
 -------------------
 -- Miscellaneous --
 -------------------
 
+local editor_automation = augroup("EditorAutomation", { clear = false })
+
 -- Open help windows vertically
 autocmd("FileType help", {
-	command = [[ wincmd L ]]
+	command = [[ wincmd L ]],
+	group = editor_automation
 })
 
 -- Remove all trailing whitespace and trim blank lines at the end of the file
 autocmd("BufWritePre", {
-	command = [[ %s/\s\+$//e | %s/\($\n\s*\)\+\%$//e ]]
+	command = [[ %s/\s\+$//e | %s/\($\n\s*\)\+\%$//e ]],
+	group = editor_automation
 })
-
--- Don't auto comment new lines (when open command is used)
-autocmd("BufEnter", { command = [[ set formatoptions-=o ]] })
 
 -- Remember last position and foldings
 autocmd("BufWinEnter", {
-	command = [[ silent! loadview ]]
+	command = [[ silent! loadview ]],
+	group = editor_automation
 })
 autocmd("BufWinLeave", {
-	command = [[ mkview ]]
+	command = [[ mkview ]],
+	group = editor_automation
+})
+
+-- Don't auto comment new lines (when open command is used)
+autocmd("BufEnter", {
+	command = [[ set formatoptions-=o ]],
+	group = editor_automation
 })
 
 -- Packer
@@ -109,11 +135,9 @@ autocmd("BufWritePost", {
 	-- Regenerate compiled loader file and load Packer everytime "plugins.lua"
 	-- is written
 	pattern = "plugins.lua",
-	command = [[ PackerCompile ]]
+	command = [[ PackerCompile ]],
+	group = editor_automation
 })
-
--- Run LOVE project
-usercmd("RunLove", "silent !love %:p:h", {})
 
 -- Toggle auto-formatting with nice defaults
 usercmd("EnableFo", [[ set formatoptions=aw2tnp |
