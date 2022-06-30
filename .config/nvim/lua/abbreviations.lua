6f8d5fd711f2d8839ca762f441bae004ba881722
Wesley Jr <wesleyjr2002@gmail.com>
2022-06-30 10:22:08 -0300

    Finally add some abbreviations in `Neovim`

--- .config/nvim/lua/abbreviations.lua
+++ .config/nvim/lua/abbreviations.lua
@@ -9,3 +9,10 @@
    github.com/wesleyjrz
    gitlab.com/wesleyjrz
 --]]
+
+-- General abbreviations
+vim.cmd([[
+	iab wj Wesley Jr.
+	iab wsjr Wesley V. Santos Jr.
+	iab copyr Copyright © <year> - All Rigths Reserved.
+]])
